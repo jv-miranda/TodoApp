c8d50d2e47c12e0569775e6c99985abb0b407295
jv-miranda <joao.vitor.miranda.siqueira@gmail.com>
2022-06-28 18:37:39 -0300

    Main functionalities and Mobile/Tablet Layout finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,22 @@
-/* This variable (idGenerated) becomes the new todo id everytime a new Todo is created and then it's incremented so that each todo can be acessed by a different id to delete it or to toggle classes.*/
-let idGenarated = 1 
+let idGenarated = 1
+
+function updateTodosCounter() {
+  const todos = document.querySelectorAll(`.card`)
+  let activeTodos = []
+  for (let todo of todos) {
+    if (todo.classList.contains(`active`)) {
+      activeTodos.push(todo)
+    }
+  }
+  const todosCounter = activeTodos.length
+  counter.innerHTML = todosCounter
+}
 
 todoInput.addEventListener(`keypress`, e => {
   if (e.key === `Enter`) {
     addTodoToHtml(todoInput.value)
     idGenarated++
+    updateTodosCounter()
   }
 })
 
@@ -12,15 +24,14 @@ function addTodoToHtml(todoText) {
   if (todoText !== ``) {
     const todosDiv = document.querySelector(`.todos`)
     const newTodo = document.createElement(`div`)
+    newTodo.classList.add(`card`, `active`)
+    newTodo.id = `card${idGenarated}`
     newTodo.innerHTML = `
-    <div class="card active" id="card${idGenarated}">
-      <div>
-        <input type="checkbox" onclick="toggleCompletedClass("${idGenarated}")/>
-        <p class="todo_text">${todoText}</p>
-      </div>
-
-      <button><img src="./assets/svgs/icon-cross.svg" alt="" /></button>
+    <div>
+      <input type="checkbox" onclick="toggleCompletedClass('card${idGenarated}')">
+      <p class="todo_text">${todoText}</p>
     </div>
+    <button onclick="deleteTodo('card${idGenarated}')"><img src="./assets/svgs/icon-cross.svg" alt=""></button>
   `
     todosDiv.appendChild(newTodo)
     todoInput.value = ``
@@ -30,5 +41,51 @@ function addTodoToHtml(todoText) {
 function toggleCompletedClass(cardId) {
   const card = document.querySelector(`#${cardId}`)
   card.classList.toggle(`active`)
-  card.classList.toggle(`completed`)
+
+  if (card.classList.contains(`completed`)) {
+    card.classList.remove(`completed`)
+    updateTodosCounter(`increment`)
+  } else {
+    card.classList.add(`completed`)
+    updateTodosCounter(`decrement`)
+  }
+
+  updateTodosCounter()
+}
+
+function deleteTodo(cardId) {
+  const card = document.querySelector(`#${cardId}`)
+  card.remove()
+
+  updateTodosCounter()
+}
+
+function clearCompletedTodos() {
+  const todos = document.querySelectorAll(`.card`)
+  for (let todo of todos) {
+    if (todo.classList.contains(`completed`)) {
+      todo.remove()
+    }
+  }
+
+  updateTodosCounter()
+}
+
+function filterTodos(HtmlClass) {
+  const todos = document.querySelectorAll(`.card`)
+  for (let todo of todos) {
+    if (!todo.classList.contains(`${HtmlClass}`)) {
+      todo.style.display = `none`
+    } else {
+      todo.style.display = `flex`
+    }
+  }
+}
+
+function addOnClass(todoId) {
+  allButton.classList.remove(`on`)
+  activeButton.classList.remove(`on`)
+  completedButton.classList.remove(`on`)
+
+  todoId.classList.add(`on`)
 }
